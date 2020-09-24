32911e92599ffb682aafcee43b8c6fe23daa1315
smr1020 <smr1020@gmail.com>
2020-09-24 15:22:28 -0400

    working models, more training data

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
+import datetime
 import glob
+import os
 
+import matplotlib.pyplot as plt 
+import numpy as np
 import pandas as pd
 import scipy.stats as stats
-import matplotlib.pyplot as plt 
-
+import tensorflow as tf
 from tqdm import tqdm
-
-
 
 def load_csv(path):
     
@@ -18,17 +19,17 @@ def load_csv(path):
 
     for filename in tqdm(all_files):
         df = pd.read_csv(filename, index_col=None, header=0)
-        df['Speed'] = df['Speed'][df['Speed'] !=  'Bronx'].astype(float)
-        df['DataAsOf'] = df['DataAsOf'][df['Speed'] !=  'Bronx'].astype(str)
+        df.columns = ['Id', 'Speed', 'TravelTime', 'Status', 'DataAsOf', 'linkId']
+        if df['Speed'].dtype != np.float64:
+            cond = ((df['Speed'].str.contains('Speed')) | (df['Speed'].str.contains('Bronx')))
+            df[cond]=np.nan
+        df['Speed'] = df['Speed'].astype(float)
+        df['DataAsOf'] = df['DataAsOf'].astype(str)
         df = df[['Speed','DataAsOf','Id']]
         li.append(df)
-        
+
 
     return pd.concat(li, axis=0, ignore_index=True)
-
-# def clean_df(df):
-    
-#     return print('nothing')
 
 def plot_traffic_speed(df,start,end,figsize = (10,3),ticks='1D'):
     
@@ -71,9 +72,22 @@ def plot_traffic_speed(df,start,end,figsize = (10,3),ticks='1D'):
     
     return df_rs
 
+def train_test_val_split(df):
+
+    column_indices = {name: i for i, name in enumerate(df.columns)}
+
+    n = len(df)
+    train_df = df[0:int(n*0.7)]
+    val_df = df[int(n*0.7):int(n*0.9)]
+    test_df = df[int(n*0.9):]
+
+    num_features = df.shape[1]
+    
+    return (train_df,val_df,test_df), num_features
+
 class WindowGenerator():
     def __init__(self, input_width, label_width, shift,
-                   train_df=train_df, val_df=val_df, test_df=test_df,
+                   train_df, val_df, test_df,
                    label_columns=None):
         # Store the raw data.
         self.train_df = train_df
@@ -108,4 +122,76 @@ class WindowGenerator():
             f'Input indices: {self.input_indices}',
             f'Label indices: {self.label_indices}',
             f'Label column name(s): {self.label_columns}'])
+    
+    def split_window(self, features):
+        
+#         inds = np.where(~np.isnan(features).any(axis=2)[:,1])[0]
+#         features = tf.convert_to_tensor(features.numpy()[inds,])
 
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack([labels[:, :, self.column_indices[name]] for name in self.label_columns],
+                              axis=-1)
+
+        # Slicing doesn't preserve static shape information, so set the shapes
+        # manually. This way the `tf.data.Datasets` are easier to inspect.
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+        
+
+        return inputs, labels
+    
+    def make_dataset(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+                        data=data,
+                        targets=None,
+                        sequence_length=self.total_window_size,
+                        sequence_stride=1,
+                        shuffle=True,
+                        batch_size=32,)
+
+        ds = ds.map(self.split_window)
+
+        return ds
+    
+    @property
+    def train(self):
+        return self.make_dataset(self.train_df)
+
+    @property
+    def val(self):
+        return self.make_dataset(self.val_df)
+
+    @property
+    def test(self):
+        return self.make_dataset(self.test_df)
+
+    @property
+    def example(self):
+        """Get and cache an example batch of `inputs, labels` for plotting."""
+        result = getattr(self, '_example', None)
+        if result is None:
+            # No example batch was found, so get one from the `.train` dataset
+            result = next(iter(self.train))
+            # And cache it for next time
+            self._example = result
+        return result
+
+
+def compile_and_fit(model, window, patience=2,MAX_EPOCHS = 20):
+    
+    
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                      patience=patience,
+                                                      mode='min')
+
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                  optimizer=tf.optimizers.Adam(),
+                  metrics=[tf.metrics.MeanAbsoluteError()])
+
+    history = model.fit(window.train, epochs=MAX_EPOCHS,
+                        validation_data=window.val,
+                        callbacks=[early_stopping])
+    return history
