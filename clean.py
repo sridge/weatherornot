1e52229b4a354b5a93064ae2f02d3f74c3def389
smr1020 <smr1020@gmail.com>
2020-10-02 15:52:27 -0400

    clean functions

--- clean.py
+++ clean.py
@@ -7,10 +7,6 @@ import scipy.stats as stats
 import utils
 
 
-
-
-
-
 def subset_data(path,boro_sel,link_id_path='linkIds.csv'):
     """Takes a subset of the NYC traffic speed sensor network, by 
     borough andby average speed
@@ -48,16 +44,18 @@ def subset_data(path,boro_sel,link_id_path='linkIds.csv'):
 
     return df
 
-def clean(year = 2019,
-    path = f'../nyc_speed_data/*{year}.csv',
-    boro_sel = ['Manhattan','Staten Island','Queens','Bronx','Brooklyn'],
-    window = '2H',freq = '15min',
-    ):
+def resample_sensors(df):
+    """resample each sensor as the average over 15 minute intervals
 
-    df = subset_data(path,boro_sel)
-    
-    # resample each sensor as the average over 15 minute intervals
-    # combine into a dataframe with linkId as the column and time as the index 
+    Args:
+        df (pandas.DataFrame): dataframe containing all sensor data at all locations
+
+    Returns:
+        df_rs (pandas.DataFrame): resampled dataframe with linkId as the 
+        column and time as the index
+
+    """ 
+
     df_list = []
     for group in df.groupby('linkId'):
         df_tem = group[1].set_index('DataAsOf')
@@ -67,6 +65,51 @@ def clean(year = 2019,
         df_list.append(df_i)
 
     df_rs = pd.concat(df_list, axis=1, ignore_index=False)
+
+    return df_rs
+
+def check_outages(df_rs,set_na=True):
+    """create an NYC median traffic speed index, setting times where
+     >25% of traffic speed sensors aren't reporting data as np.NaN
+
+    Args:
+        df_rs (pandas.DataFrame): dataframe containing all sensor data 
+        at all locations
+        set_na (bool): Timesteps set to np.NaN if True and >25% of
+        sensors are not reporting
+
+    Returns:
+        speed_ts (pandas.Series): timeseries of NYC median traffic 
+        speed. Some timesteps set to np.NaN if set_na=True and >25% of
+        sensors are not reporting
+
+    """ 
+
+    sensor_outage = df_rs.isna().sum(axis=1)
+    tol = 153-(153*0.75)
+
+    cond_toss = sensor_outage>tol
+
+    speed_ts = df_rs.median(axis=1)
+
+    if set_na:
+        speed_ts[cond_toss] = np.nan
+
+    return cond_toss,speed_ts
+
+def clean(year = 2019,
+    path = f'../nyc_speed_data/*{year}.csv',
+    boro_sel = ['Manhattan','Staten Island','Queens','Bronx','Brooklyn'],
+    window = '2H',freq = '15min',
+    ):
+
+    df = subset_data(path,boro_sel)
+    
+    df_rs = sample_sensors(df)
+
+    cond_toss,speed_ts = check_outages(df_rs)
+
+
 
     return df  
 
