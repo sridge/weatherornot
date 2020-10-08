d8b03534a10c60a852e43889b9bff462b09a6610
smr1020 <smr1020@gmail.com>
2020-10-07 23:12:09 -0400

    fix setup

--- api/src/setup.py
+++ api/src/setup.py
@@ -1,3 +1,11 @@
 import setuptools
+from pip.req import parse_requirements
 
-setuptools.setup(name="src",version="0.0.1")
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(<requirements_path>)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+setuptools.setup(name="src",version="0.0.1",install_requires=reqs)
