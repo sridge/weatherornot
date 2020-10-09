f64b59603becc2b337af89fc3e09d60f1f72c230
smr1020 <smr1020@gmail.com>
2020-10-08 21:17:13 -0400

    add clock process

--- clock.py
+++ clock.py
@@ -1,10 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
+import forecast
+
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('forecast starting')
-    import forecast
     forecast.run_forecast_system()
     print('forecast finished')
     
