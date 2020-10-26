7beb91b057a247f662fc0f21e3851e4b8b51cd8d
smr1020 <smr1020@gmail.com>
2020-10-25 21:06:21 -0400

    check dir

--- clock.py
+++ clock.py
@@ -2,11 +2,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 import forecast
+import os
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('forecast starting')
-    forecast.run_forecast_system()
+    print(os.getcwd())
     print('forecast finished')
     
 
