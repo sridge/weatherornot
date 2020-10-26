ac5607937bfbb59d932c94ce0f054b2903cd7d01
smr1020 <smr1020@gmail.com>
2020-10-26 16:32:48 -0400

    set clock to test interval

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ sched = BlockingScheduler()
 import forecast
 import os
 
-@sched.scheduled_job('interval', minutes=15)
+@sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('forecast starting')
     forecast.run_forecast_system()
