e947e2fd21d32263531a668ae8048be0c2444995
smr1020 <smr1020@gmail.com>
2020-10-27 09:24:04 -0400

    update forecast image

--- clock.py
+++ clock.py
@@ -9,10 +9,5 @@ def timed_job():
     print('forecast starting')
     forecast.run_forecast_system()
     print('forecast finished')
-    
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
 
 sched.start()
