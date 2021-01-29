70421be339f1eb728a59847bc72ea65d6093969c
sridge <smr1020@gmail.com>
2021-01-29 16:00:43 -0500

    change scheduler print formatting

--- clock.py
+++ clock.py
@@ -5,8 +5,12 @@ import forecast
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
+    print('-----------------------')
     print('forecast starting')
+    print('-----------------------')
     forecast.run_forecast_system()
+    print('-----------------------')
     print('forecast finished')
+    print('-----------------------')
 
 sched.start()
